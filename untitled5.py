d647f04b398c6698b089caeb677685adf03b5583
Nishant8 <agarwalnishant27@gmail.com>
2020-09-05 22:18:47 +0530

    Class 1 - 05/09/2020

---
+++ untitled5.py
@@ -0,0 +1,361 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  5 20:06:28 2020
+
+@author: agarw
+"""
+
+print?
+
+print("Nishant Agarwal")
+
+    
+print("Nishant","Agarwal",sep='***')
+
+print("Nishant","Agarwal",sep='***', end='**')
+
+help("print")
+
+help(print)
+
+help("OPERATORS")
+
+A=10
+print(A)
+
+A<10
+print(A)
+
+!pip install pandas
+!python --version
+!pip list
+
+input("Your Country ")
+
+A=10
+B=input("Your Country ")
+
+A=30
+
+A="Vikas"
+B=10
+C=30.33
+
+type(A)
+type(B)
+type(C)
+
+S=input("Nishant ")
+print(S)
+type(S)
+
+Number=input()
+print(Number)
+type(Number)
+
+Num=int(Number)
+type(Num)
+
+Number
+Num
+
+Num="Nishant"
+
+print("I Love in {0}")
+
+
+PhoneNum=int(input())
+
+type(PhoneNum)
+
+
+
+
+country= "India"
+
+
+print("I Live in", country, "which is very good country")
+print("I Live in {0} which is very good country".format(country))
+print("I Live in", country)
+print("I Live in {0}".format(country))
+print("I Live in which is very good country {0}".format(country))
+
+country
+
+
+countrycode=91
+
+print("I Live in", str(countrycode),"which is very good country".format(country))
+print("I Live in {0} which is very good country is {1}".format(country,countrycode))
+
+a=1.11
+b=int(a)
+c=float(b)
+
+x=2
+y=3
+
+
+z=x+y
+
+print(z)
+print(x)
+print(y)
+
+
+print(x+y)
+print(x-y)
+print(x/y)
+print(x*y)
+
+x=5
+x=x-1
+x
+
+x-=1
+x
+
+x*=2
+x
+
+y=x**3
+y
+
+x
+x**=2
+x
+
+
+a=10
+b=11
+
+
+a/2
+b/2
+
+#ModuLo
+
+b%3
+a%3
+
+
+#BooLean
+
+'''
+
+0-9
+decimal
+
+boolean
+0-1
+False-True
+And
+Or
+Not
+
+And
+A B X
+0 0 0
+1 1 1
+1 0 0
+0 1 0
+
+
+
+Or
+A B X
+0 0 0
+1 1 1
+1 0 1
+0 1 1
+
+'''
+
+A=0
+B=1
+
+X= A and B
+X
+
+
+A=1
+B=1
+
+X= A and B
+X
+
+
+A=False
+B=True
+
+X= A and B
+X
+
+
+A=True
+B=True
+
+X= A and B
+X
+
+
+
+A=0
+B=1
+
+X= A or B
+X
+
+
+A=1
+B=1
+
+X= A or B
+X
+
+
+A=False
+B=True
+
+X= A or B
+X
+
+
+A=True
+B=True
+
+X= A or B
+X
+
+
+A=False
+B=False
+
+X= A or B
+X
+
+type(X)
+
+
+'''
+Not
+
+A X
+0 1
+1 0
+
+'''
+
+
+A=0
+
+X= not(A)
+X
+
+
+A=1
+
+X= not(A)
+X
+
+
+A=15
+bool(A)
+
+B=0
+bool(B)
+
+A=5
+B=61
+
+A and B
+X=A and B
+X
+
+
+
+Fname="Nishant"
+Lname= "Agarwal"
+RollNo=111
+
+name= Fname +" "+ Lname
+name
+
+
+name= Fname + "  " + Lname + "  " +str(RollNo) 
+name
+
+
+len(Fname)
+len(Lname)
+len(RollNo)
+
+Lname=Lname.capitalize()
+print(Lname)
+
+Lname=Lname.upper()
+print(Lname)
+
+
+Lname=Lname.lower()
+print(Lname)
+
+Lname=Lname.rjust(10)
+print(Lname)
+
+Lname=Lname.center(15)
+print(Lname)
+
+
+name
+
+
+name=name.replace('Agarwal', 'Kumar')
+name
+
+
+name=name.replace('111', '113')
+name
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
